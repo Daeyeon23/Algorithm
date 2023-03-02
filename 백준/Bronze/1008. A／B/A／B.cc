969516eaa5acf94121b249df15fa3c03c08ad6fa
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-03-02 14:01:40 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+using namespace std;
+
+
+
+int main() {
+
+	double a, b;
+	cin >> a >> b;
+
+	cout.precision(10);
+	cout << a / b;
+
+
+	return 0;
+}
