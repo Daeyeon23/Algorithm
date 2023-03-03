320becf99af06eeaa9cd95c709703bc25dfcd636
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-03-03 17:50:34 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+using namespace std;
+
+
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	for (int i = 1;i <= N;i++) {
+		cout << i << '\n';
+	}
+	return 0;
+}
