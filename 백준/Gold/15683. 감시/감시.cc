d14c442f63d39dddc43b626a85fb064d7ee6a59f
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-03-19 12:47:31 +0900

    [Gold IV] Title: 감시, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -14,10 +14,11 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cstring>
 using namespace std;
 
-int diry[4] = { -1, 0, 1, 0 };  // 상 우 하 좌 순서
-int dirx[4] = { 0, 1, 0, -1 };
+int diry[4] = {-1, 0, 1, 0}; // 상 우 하 좌 순서
+int dirx[4] = {0, 1, 0, -1};
 
 int N; // 세로
 int M; // 가로
@@ -31,7 +32,7 @@ vector<node> v;
 int cctvnum;    // cctv 수
 int ans = 21e8; // 최소 개수
 
-int CheckZero()
+int CheckZero() // 0 개수 세기
 {
     int cnt = 0;
     for (int i = 0; i < N; i++)
@@ -53,7 +54,7 @@ void CheckLine(int y, int x, int dir)
     {
         dy += diry[dir];
         dx += dirx[dir];
-        if (dy >= N || dy < 0 || dx >= M || dx < 0)
+        if (dy >= N || dy < 0 || dx >= M || dx < 0) // 경계면
             break;
         if (Map[dy][dx] == 6)
             break; // 벽이면 멈추기
@@ -62,7 +63,6 @@ void CheckLine(int y, int x, int dir)
         Map[dy][dx] = 1; // 감시 체크
     }
 }
-
 
 void dfs(int level)
 {
@@ -74,94 +74,67 @@ void dfs(int level)
 
     int cctv = v[level].num;
 
-    int saveMap[10][10] = { 0 }; // 맵 저장
+    int SaveMap[10][10] = {0}; // 맵 저장
 
     if (cctv == 1)
     {
         for (int dir = 0; dir < 4; dir++)
         {
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    saveMap[i][j] = Map[i][j];
-                }
-            }
+
+            memcpy(SaveMap, Map, sizeof(Map));
+
             CheckLine(v[level].y, v[level].x, dir);
             dfs(level + 1);
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    Map[i][j] = saveMap[i][j];
-                }
-            }
+
+            memcpy(Map, SaveMap, sizeof(SaveMap));
         }
     }
     else if (cctv == 2)
     {
-        for (int dir = 0; dir < 2; dir++)  // dir 0, 1만
+        for (int dir = 0; dir < 2; dir++) // dir 0, 1만
         {
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    saveMap[i][j] = Map[i][j];
-                }
-            }
+            memcpy(SaveMap, Map, sizeof(Map));
+
             CheckLine(v[level].y, v[level].x, dir);
             CheckLine(v[level].y, v[level].x, dir + 2);
 
             dfs(level + 1);
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    Map[i][j] = saveMap[i][j];
-                }
-            }
+
+            memcpy(Map, SaveMap, sizeof(SaveMap));
         }
     }
     else if (cctv == 3)
     {
         for (int dir = 0; dir < 4; dir++)
         {
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    saveMap[i][j] = Map[i][j];
-                }
-            }
+            memcpy(SaveMap, Map, sizeof(Map));
+
             CheckLine(v[level].y, v[level].x, dir);
-            CheckLine(v[level].y, v[level].x, (dir + 1)%4);
+            CheckLine(v[level].y, v[level].x, (dir + 1) % 4);
 
             dfs(level + 1);
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    Map[i][j] = saveMap[i][j];
-                }
-            }
+
+            memcpy(Map, SaveMap, sizeof(SaveMap));
         }
     }
     else if (cctv == 4)
     {
         for (int dir = 0; dir < 4; dir++)
         {
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    saveMap[i][j] = Map[i][j];
-                }
-            }
+            memcpy(SaveMap, Map, sizeof(Map));
+
             CheckLine(v[level].y, v[level].x, (dir + 3) % 4);
             CheckLine(v[level].y, v[level].x, dir);
             CheckLine(v[level].y, v[level].x, (dir + 1) % 4);
 
             dfs(level + 1);
-            for (int i = 0;i < N;i++) {
-                for (int j = 0;j < M;j++) {
-                    Map[i][j] = saveMap[i][j];
-                }
-            }
+
+            memcpy(Map, SaveMap, sizeof(SaveMap));
         }
     }
     else if (cctv == 5)
     {
-        for (int i = 0;i < N;i++) {
-            for (int j = 0;j < M;j++) {
-                saveMap[i][j] = Map[i][j];
-            }
-        }
+        memcpy(SaveMap, Map, sizeof(Map));
 
         CheckLine(v[level].y, v[level].x, 0);
         CheckLine(v[level].y, v[level].x, 1);
@@ -169,11 +142,8 @@ void dfs(int level)
         CheckLine(v[level].y, v[level].x, 3);
 
         dfs(level + 1);
-        for (int i = 0;i < N;i++) {
-            for (int j = 0;j < M;j++) {
-                Map[i][j] = saveMap[i][j];
-            }
-        }
+
+        memcpy(Map, SaveMap, sizeof(SaveMap));
     }
 }
 
@@ -188,14 +158,14 @@ void init()
             cin >> Map[i][j];
             if (Map[i][j] >= 1 && Map[i][j] < 6)
             {
-                v.push_back({ i, j, Map[i][j] });
+                v.push_back({i, j, Map[i][j]});
             }
         }
     }
     cctvnum = v.size();
 }
 
-int main()
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
