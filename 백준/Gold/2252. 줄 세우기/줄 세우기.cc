35b28de9dbbb0d456566340eefa86394a810acd6
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-03-19 20:42:51 +0900

    [Gold III] Title: 줄 세우기, Time: 456 ms, Memory: 4084 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int N, M;
+vector<int> v[32111];
+int DAT[32111];
+int visited[32111];
+
+void init() {
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+		int from, to;
+		cin >> from >> to;
+		v[from].push_back(to);
+		DAT[to]++;
+	}
+}
+
+void solve() {
+
+	queue<int> q;
+	int cnt = 0;
+	while (1) {
+
+
+		for (int i = 1; i <= N; i++) {
+			if (DAT[i] == 0 && visited[i]==0) {
+				q.push(i);
+				visited[i] = 1;
+				break;
+			}
+		}
+
+		while (!q.empty()) {
+			int now = q.front();
+			q.pop();
+
+			cout << now << ' ';
+			cnt++;
+
+			int len = v[now].size();
+			for (int i = 0; i < len; i++) {
+				DAT[v[now][i]]--;
+				if (DAT[v[now][i]] == 0 && visited[v[now][i]]==0) {
+					q.push(v[now][i]);
+					visited[v[now][i]] = 1;
+				}
+			}
+		}
+		if (cnt == N)break;
+	}
+
+}
+
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	init();
+
+	solve();
+
+	return 0;
+}
