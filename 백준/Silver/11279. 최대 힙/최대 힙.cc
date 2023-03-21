7d935820359c8483c3ebececcc49ccfa7eee3b71
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-03-21 13:19:14 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+struct cmd {
+	bool operator () (int left, int right) {
+		return left < right;
+	}
+};
+
+priority_queue<int, vector<int>,cmd> pq;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0;i < N;i++) {
+		int temp;
+		cin >> temp;
+
+		if (temp == 0) {
+			if (pq.empty()) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(temp);
+		}
+	}
+
+
+
+	return 0;
+}
