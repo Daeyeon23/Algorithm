3192c59231046bf2212d66f4593811d67a5153a5
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-04-04 23:11:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 36 ms, Memory: 41480 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,40 +1,40 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-int N, K;
-struct node {
-	int weight;
-	int cost;
-};
-node arr[111];
+int N;	// 물품 수
+int K;	// 버틸 수 있는 무게
+int arr[101][2];	// 무게, 가치 정보
 int DP[101][100010];
 
-int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	
+void init() {
 	cin >> N >> K;
+	for (int i = 1; i <= N; ++i) {
+		cin >> arr[i][0] >> arr[i][1];
+	}
+}
+
+int solve() {
 
 	for (int i = 1; i <= N; ++i) {
-		int w, c;
-		cin >> w >> c;
-		arr[i].weight = w;
-		arr[i].cost = c;
-	}
-
-	for (int i = 1; i <= K; ++i) {
-		for (int j = 1; j <= N; ++j) {
-			if (arr[j].weight > i) {
-				DP[j][i] = DP[j - 1][i];
+		for (int k = 1; k <= K; ++k) {
+			if (k < arr[i][0]) {	// 이번 물건을 못 넣을때.
+				DP[i][k] = DP[i - 1][k];
 			}
 			else {
-				DP[j][i] = max(DP[j - 1][i - arr[j].weight] + arr[j].cost, DP[j - 1][i]);
+				DP[i][k] = max(DP[i - 1][k - arr[i][0]] + arr[i][1], DP[i - 1][k]);
 			}
 		}
 	}
 
-	cout << DP[N][K];
+	return DP[N][K];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	init();
+	cout << solve() << '\n';
+
 	return 0;
 }
