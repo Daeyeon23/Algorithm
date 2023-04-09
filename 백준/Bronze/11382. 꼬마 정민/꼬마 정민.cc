e413b6861f2998ba28424b27f081819d0ac4c425
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-04-09 23:29:54 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	long long a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+	return 0;
+}
