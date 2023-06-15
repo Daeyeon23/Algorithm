6a4d5c1a319d3b01cdde4c351b6703b5f576609a
Daeyeon_Kim <110017794+Daeyeon23@users.noreply.github.com>
2023-06-16 00:14:43 +0900

    [Silver III] Title: 주유소, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dist[100000];
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N-1; ++i) {
+		cin >> dist[i];
+	}
+
+	int Min = 21e8;
+	int ans = 0;
+	for (int i = 0; i < N; ++i) {
+		int cost;
+		cin >> cost;
+		if (i == N - 1) {
+			cout << ans;
+			return 0;
+		}
+
+		Min = min(Min, cost);
+
+		ans += Min * dist[i];
+	}
+
+
+
+	return 0;
+}
